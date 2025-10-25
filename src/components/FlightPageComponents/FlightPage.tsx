8e11de2e39c8c9fe04408be4ec548779d2d1a0bb
Alexandre Mathurin <35438645+ToxicBowl@users.noreply.github.com>
2025-10-25 00:23:23 +0000

    Created a folder for all things related to the flight page for the sake of organization

--- src/components/FlightPageComponents/FlightPage.tsx
+++ src/components/FlightPageComponents/FlightPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Navbar from "./Navbar";
+import Navbar from "../Navbar";
 
 const FlightPage: React.FC = () => {
   const [tripType, setTripType] = useState<"oneway" | "roundtrip">("roundtrip");
